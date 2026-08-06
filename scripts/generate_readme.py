0fcffdc43f0bbf6f1904017566cb92377486a3f7
Robert Malleschitz <robert.malleschitz@novotechnik.de>
2026-08-06 15:15:49 +0200

    (m) Update generate_readme.py
    Erster Versuch ein wenig mehr Struktur rein zubekommen.

--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-## @package generate_readme.py ###################################################
+## @package generate_readme.py #################################################
 ##
 ## @brief    Erzeugt die README.md, welche als Startseite/Overwiew unserers
 ##           Novotechnik GutHub dargestellt wird.
@@ -8,6 +8,7 @@
 ## @author   Robert Malleschitz
 ## @mail     robert.malleschitz@novotechnik.de
 ################################################################################
+
 # ==============================================================================
 # Imports
 # ==============================================================================
@@ -15,15 +16,218 @@ import os
 import json
 import subprocess
 from collections import defaultdict
+from pathlib import Path
+from typing import Any
+
 # ==============================================================================
 # Variables
 # ==============================================================================
-ogranization = "Novotechnik"    #!< Entsprechend der GitHub Ogranisation.
-githubCheckoutCommand = ""
+CATEGORY_ORDER = [
+    "Firmware",
+    "Jenkins",
+    "Docker",
+    "Tools",
+    "Extern",
+    "Sonstiges",
+    "Quality"
+]
+
+CATEGORY_ICONS = {
+    "Firmware"  : "🚀",
+    "Jenkins"   : "🤖",
+    "Docker"    : "🐳",
+    "Tools"     : "🛠️",
+    "Extern"    : "🌐",
+    "Sonstiges" : "📌",
+    "Quality"   : "✔️"
+}
+
+ORGANIZATION = "Novotechnik"    #!< Entsprechend der GitHub Ogranisation.
 
 # ==============================================================================
 # Functions
 # ==============================================================================
+def get_repositories(_organization: str = "") -> list:
+    '''
+    @brief    Liest alle Repositories der übergebenen Organisation aus.
+
+    @param [in]    '_organization'    Bezeichner, wie er auch in GitHub steht.
+
+    @return  'JSON'    Enthält alle Repositories zur übergebenen (GitHub) Or-
+                       ganisation.
+    '''
+
+    githubCommand = [
+        "gh",
+        "repo",
+        "list",
+        _organization,
+        "--limit",
+        "100",
+        "--json",
+        "name,description"
+    ]
+
+    repos = subprocess.check_output(githubCommand, text=True)
+    reposJSON = json.loads(repos)
+
+    return reposJSON
+
+
+def get_repository_category(_repository: str = "") -> str:
+    '''
+    @brief    Liest die Custom Property 'Kategorie' des übergebenem Repositories
+              aus.
+
+    @param [in]    '_repository'      Wir wollen die Proberties dieses Repos 
+                                      haben!
+
+    @return  'String'    Proberty Daten.
+    '''
+
+    githubCommand = [
+        "gh",
+        "api",
+        f"repos/{ORGANIZATION}/{_repository}"
+    ]
+
+    repoData = subprocess.check_output(githubCommand, text=True)
+    repoDataJSON = json.loads(repoData)
+
+    properties = (
+                    repoDataJSON
+                    .get("custom_properties", {})
+                    .get("Kategorie", "Sonstiges")
+    )
+    
+    return properties
+
+
+def group_repositories_by_category(_repositories: list[dict]) -> defaultdict[str, list[dict]]:
+    '''
+    @brief    Gruppiert Repositories anhand der Kategorie.
+
+    @param [in]    '_repositories'    Diese Repos sollen alle gruppiert werden.
+
+    @return  'defaultdict[str, list[dict]]'    Verzeichnis mit den gruppierten
+                                               Repos
+    '''
+
+    grouped = defaultdict(list)
+
+    for repository in _repositories:
+        category = get_repository_category(repository["name"])
+        grouped[category].append(repository)
+
+    return grouped
+
+
+def load_template() -> str:
+    '''
+    @brief    Lädt das README.md Template.
+
+    @param  'None'
+
+    @return  'String'    README.md Template
+    '''
+
+    readmeTemplate = Path("scripts/templates/README.md.template")
+
+    template = readmeTemplate.read_text(encoding="utf-8")
+
+    return template
+
+
+def create_repository_list(_repositories: list[dict] | None = None) -> str:
+    '''
+    @brief    Aus einer Liste von GitHub-Repositories wird nun eine formatierte
+              Markdown-Liste als String erzeugt
+
+    @param [in]    '_repositories'    Wir wollen die Proberties dieses Repos 
+                                      haben!
+    
+    return  'String'    In Form gebrachte Repository Darstellung.
+    '''
+
+    if _repositories is None:
+        return ""
+
+    lines = []
+
+    for repository in sorted(
+            _repositories,
+            key=lambda repo: repo["name"].lower()
+    ):
+        name = repository["name"]
+        description = repository.get("description", "")
+
+        lines.append(
+            f"- [{name}]"
+            f"(https://github.com/{ORGANIZATION}/{name})"
+        )
+
+        if description:
+            lines.append(
+                f"  - {description}"
+            )
+
+    repoList = "\n".join(lines)
+
+    return repoList 
+
+
+def render_template(
+                _template: str,
+                    _groupedRepos: defaultdict[str, list[dict]]) -> str:
+    '''
+    @brief    Ersetzt die Platzhalter im Template.
+
+    @param [in]    '_template'        Diese Template wollen wir befüllen.
+    @param [in]    '_groupedRepos'    Mit diesen gruppierten Repos wollen wir
+                                      das Template befüllen.
+    
+    @return  'String'    Vorlage zur Darstellung in GitHub/Novotechnik/Overiew.
+    '''
+
+    overviewPage = _template
+
+    for category, icon in CATEGORY_ICONS.items():
+        placeholder = (
+            "{{"
+            + category.upper()
+            + "}}"
+        )
+
+        repositories = _groupedRepos.get(category, [])
+        section = (f"## {icon} {category}\n\n")
+
+        if repositories:
+            section += create_repository_list(repositories)
+        else:
+            section += "_Keine Einträge_"
+
+        overviewPage = overviewPage.replace(placeholder, section)
+    
+    return overviewPage
+
+
+def write_readme(_content: str = "") -> None:
+    '''
+    @brief    Schreibt die fertige REDME.md.
+
+    @note     Diese muss zwingend unter 
+              "https://github.com/Novotechnik/.github/tree/main/profile" liegen!
+
+    @sideeffect    README.md wird überschrieben.
+
+    @param [in]    '_content'    README.md wollen wir hiermit überschreiben.
+
+    @return  'None'
+    '''
+
+    readmeFile = Path("profile/README.md")
+
+    readmeFile.write_text(_content, encoding="utf-8")
 
 # ==============================================================================
 # Classes
@@ -33,46 +237,20 @@ githubCheckoutCommand = ""
 # Script
 # ==============================================================================
 
-repos = subprocess.check_output([
-    "gh",
-    "repo",
-    "organization",
-    "--limit",
-    "--json",
-    "name"
-])
+def main():
+    '''
+    @brief    Hier lesen wir alle verfügbaren Repositories ein, die in unserem
+              Organisations (Novotechnik) GitHub angelegt sind.
+    
+    @param  'None'
 
-categories = defaultdict(list)
-
-for repo in repos:
-    name = repo["name"]
-
-    try:
-        output = subprocess.check_output([
-            "gh",
-            "api",
-            "f"repos/{orgranization}/{name}/properties/values"
-        ])
-        values = json.loads(output)
-        category = "Sonstiges"
-
-        for value in values:
-            if "Kategorie" == value["property_name"]
-                category = value["value"]
-        categories[category].append(name)
-
-    except Exception:
-        categories["Sonstoges"].append(name)
-      
-with open("profile/README.md", "w") as readme:
-    readme.write("# Novotechnik Repositories\n\n")
-
-    for category in sorted(categories):
-        readme.write(f"## {category}\n\n)
-
-        for repo in sorted(categories[category]):
-          readme.write(f"- https://gihtub.com/{organization}/{repo}\n")
-        readme.write("\n")
+    @return "repos/Novotechnik/.github/profile/README.md"
+    '''
+    repositories = get_repositories(ORGANIZATION)
+    groupedRepositorie = group_repositories_by_category(repositories)
+    template = load_template()
+    overviewPage = render_template(template, groupedRepositories)
+    write_readme(overviewPage)
 
 if __name__ == "__main__":
     main()
